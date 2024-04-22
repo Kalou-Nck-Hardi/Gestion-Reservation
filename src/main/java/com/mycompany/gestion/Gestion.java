3d3de968b36618e2143fa29b57846eab5c995152
hardi <hervehardi@gmail.com>
2024-04-22 14:07:59 +0100

    first commit

---
+++ src/main/java/com/mycompany/gestion/Gestion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion;
+
+/**
+ *
+ * @author G17
+ */
+public class Gestion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
